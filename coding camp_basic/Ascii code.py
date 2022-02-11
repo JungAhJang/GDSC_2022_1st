2fc5ff39f5eaa76ea737b38b681c32ccb91a98c9
Jung Ah <86693895+JungAhJang@users.noreply.github.com>
2022-02-11 17:09:03 +0900

    confirm

---
+++ coding camp_basic/Ascii code.py
@@ -0,0 +1,12 @@
+# arr = list(map(int,input().split()))
+# elem_asc=0
+
+# for elem in arr :
+#     elem_asc = chr(elem)
+#     print(elem_asc, end=" ")
+
+
+
+n,arr = tuple(input().split())
+
+print(arr)
