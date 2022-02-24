8b84adaa5ed8fe12d69067119022de1ed41178af
JunAhJang <junga5853@naver.com>
2022-02-24 09:31:47 +0900

    변수의 영역

--- coding camp_basic/부분문자열의위치.py
+++ coding camp_basic/부분문자열의위치.py
@@ -1,26 +1,20 @@
-#주어진 입력 문자열에 대하여 목적 문자열이 부분 문자열로 존재하는 경우, 부분 문자열의 시작 인덱스를 출력하는 코드를 작성해보세요. 단, 함수를 이용하여 문제를 해결해주세요. 인덱스는 0부터 시작한다고 가정합니다.
-
 string = input()
 find_string = input()
-check = False
-cnt = 0
 
-def find() :
-    global check, cnt
-    for i in range(len(string)) : #입력받은 문자열만큼 반복
-        if string[i] == find_string[0] :  #입력받은 문자열의 i번째 원소와 찾고자 하는 문자열의 i번째 원소가 같다면
-            for j in range(len(find_string)) : #찾고자하는 문자열의 길이만큼 반복
-                if string[i+j] == find_string[j] : #찾고자 하는 문자열길이만큼 반복해 부분문자열이 맞나 체크
-                    cnt += 1 
-            if cnt == len(find_string) : #찾고자 하는 부분 문자열 길이만큼 같다면
-                check = True
-                idx = i #처음 인덱스를 저장
-            else :
-                cnt = 0
-                check = False
-    if check :
-        print(idx)
-    elif not check :
-        print(-1)
+#범위 판단
+def in_range(i,j,n) :
+    return i+j < n
 
-find()
+def find_idx(string,find_string) :
+    n, m = len(string) , len(find_string)
+    for i in range(n) : #입력받은 문자열만큼 반복, 시작점부터문자열길이까지 문자열이 일치하는지
+        cnt = 0
+        for j in range(m) :
+            if in_range(i,j,n) and string[i+j] == find_string[j] :
+                cnt += 1 
+        if cnt == m :
+            idx = i #시작 문자열의 위치 반환
+            return idx
+    return -1
+
+print(find_idx(string,find_string))
